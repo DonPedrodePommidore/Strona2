b84873cc38f0c375e81f40f8da06e5af50f8c47d
DonPedrodePommidore <mborek23@gmail.com>
2025-12-26 13:05:20 +0100

    Strona2_poprawki

--- FlaskProject/app.py
+++ FlaskProject/app.py
@@ -1,22 +1,26 @@
 from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 app = Flask(__name__)
-
-
-@app.route('/')
-def home():  # put application's code here
+@app.route('/', methods=['GET', 'POST'])
+def home():
     db = sqlite3.connect('movies.db')
     cursor = db.cursor()
-    cursor.execute('SELECT * FROM movies')
+    if request.method == 'POST':
+        movies_to_remove_ids = request.form.getlist('movieToRemove')
+        for id in movies_to_remove_ids:
+            cursor.execute('DELETE FROM movies WHERE rowid = ?', (id,))
+        db.commit()
+    cursor.execute('SELECT rowid, * FROM movies')
     return render_template('home.html', movies=cursor)
 @app.route('/addMovie', methods=['GET', 'POST'])
-def add_movie():  # put application's code here
+def add_movie():
     if request.method == 'POST':
         movieTitle = request.form.get('title')
         movieYear= request.form.get('year')
         movieActors= request.form.get('actors')
+        if not movieTitle:
+            return "Wprowadź nazwę filmu"
         #dodać do bazy
-        #cursor.execute(query='INSERT INTO movies (title, year, actors) VALUES ("{title}",{year},"{actors}")',)
         db = sqlite3.connect('movies.db')
         cursor = db.cursor()
         cursor.execute('INSERT INTO movies (title, year, actors) VALUES (?, ?, ?)', (movieTitle, movieYear, movieActors))
